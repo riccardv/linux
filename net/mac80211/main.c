1af8900dd161c00a0758c67f39938d6228964a7a
Ben Greear <greearb@candelatech.com>
2016-04-21 13:15:50 -0700

    wifi: Add warning messages about why registration fails.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/mac80211/main.c
+++ net/mac80211/main.c
@@ -1123,8 +1123,10 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 
 	if (ieee80211_hw_check(hw, QUEUE_CONTROL) &&
 	    (local->hw.offchannel_tx_hw_queue == IEEE80211_INVAL_HW_QUEUE ||
-	     local->hw.offchannel_tx_hw_queue >= local->hw.queues))
+	     local->hw.offchannel_tx_hw_queue >= local->hw.queues)) {
+		pr_err("queue-ctrl mismatch.\n");
 		return -EINVAL;
+	}
 
 	if ((hw->wiphy->features & NL80211_FEATURE_TDLS_CHANNEL_SWITCH) &&
 	    (!local->ops->tdls_channel_switch ||
@@ -1185,8 +1187,10 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 	}
 
 #ifdef CONFIG_PM
-	if (hw->wiphy->wowlan && (!local->ops->suspend || !local->ops->resume))
+	if (hw->wiphy->wowlan && (!local->ops->suspend || !local->ops->resume)) {
+		pr_err("wowlan mismatch.\n");
 		return -EINVAL;
+	}
 #endif
 
 	if (local->emulate_chanctx) {
@@ -1195,8 +1199,10 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 
 			comb = &local->hw.wiphy->iface_combinations[i];
 
-			if (comb->num_different_channels > 1)
+			if (comb->num_different_channels > 1) {
+				pr_err("num-diff-channels: %d > 1\n", comb->num_different_channels);
 				return -EINVAL;
+			}
 		}
 	}
 
@@ -1307,8 +1313,10 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 
 		/* HT, VHT, HE require QoS, thus >= 4 queues */
 		if (WARN_ON(local->hw.queues < IEEE80211_NUM_ACS &&
-			    (supp_ht || supp_vht || supp_he)))
+			    (supp_ht || supp_vht || supp_he))) {
+			pr_err("not enough queues for ht/vht/HE\n");
 			return -EINVAL;
+		}
 
 		/* EHT requires HE support */
 		if (WARN_ON(supp_eht && !supp_he))
@@ -1341,7 +1349,6 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 	hw->wiphy->interface_modes |= BIT(NL80211_IFTYPE_MONITOR);
 	hw->wiphy->software_iftypes |= BIT(NL80211_IFTYPE_MONITOR);
 
-
 	local->int_scan_req = kzalloc(sizeof(*local->int_scan_req) +
 				      sizeof(void *) * channels, GFP_KERNEL);
 	if (!local->int_scan_req)
@@ -1373,6 +1380,7 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 	} else if (ieee80211_hw_check(&local->hw, SIGNAL_UNSPEC)) {
 		local->hw.wiphy->signal_type = CFG80211_SIGNAL_TYPE_UNSPEC;
 		if (hw->max_signal <= 0) {
+			pr_err("max_signal: %d < 0\n", hw->max_signal);
 			result = -EINVAL;
 			goto fail_workqueue;
 		}
