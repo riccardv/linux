bb1d959b8d316f63084c3b2666b1ab89a701e1f6
Dylan Eskew <dylan.eskew@candelatech.com>
2024-09-24 12:13:10 -0700

    mac80211: add ability to ignore EDCA in stations
    For the purposes of testing - apparently common - broken
    stations.
    
    Signed-off-by: Dylan Eskew <dylan.eskew@candelatech.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -3117,6 +3117,7 @@ struct cfg80211_assoc_link {
  * @ASSOC_REQ_DISABLE_160:  Disable 160Mhz
  * @ASSOC_REQ_DISABLE_OFDMA:  Disable OFDMA
  * @ASSOC_REQ_DISABLE_320:  Disable 320Mhz
+ * @ASSOC_REQ_IGNORE_EDCA:  Ignore EDCA info
  */
 enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_HT			= BIT(0),
@@ -3131,6 +3132,7 @@ enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_160			= BIT(9),
 	ASSOC_REQ_DISABLE_OFDMA			= BIT(10),
 	ASSOC_REQ_DISABLE_320			= BIT(11),
+	ASSOC_REQ_IGNORE_EDCA			= BIT(12),
 };
 
 /**
